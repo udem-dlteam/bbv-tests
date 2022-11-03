6390464f5f05f2deb912bd322bf75f6c3adbc1f6
Manuel Serrano <Manuel.Serrano@inria.fr>
2022-11-03 16:11:46 +0100

    Fix Bigloo scripts and prelude

--- bench.sh
+++ bench.sh
@@ -2,24 +2,35 @@
 
 debug=false
 
-bench=tests/recursive/fib ; args="n: 39" ; repeat=10
+#bench=tests/recursive/fib ; args="n: 39" ; repeat=10
 #bench=tests/recursive/ack ; args="m: 3 n: 9" ; repeat=50
-#bench=tests/recursive/tak ; args="x: 18 y: 12 z: 6" ; repeat=10000
+bench=tests/recursive/tak ; args="x: 18 y: 12 z: 6" ; repeat=10000
 #bench=tests/recursive/takl ; args="x: 18 y: 12 z: 6" ; repeat=1000
 #bench=tests/recursive/diviter ; args="" ; repeat=200000
 #bench=tests/recursive/divrec ; args="" ; repeat=200000
 #bench=tests/recursive/array1 ; args="n: 200000" ; repeat=10
 
+TIME=time
+TIMEARGS=
 GCC=gcc
 
 #export WIPGAMBITDIR=`pwd`/../gambit
 
-SC_range="gambit"
+#* SC_range="gambit"                                                   */
+#* SG_range="S G"                                                      */
+#* V_range="0 1 2 3 4 5"                                               */
+#* I_range="0 350"                                                     */
+#* CC_range="$GCC clang"                                               */
+#* O_range="0 1 2 3"                                                   */
+
+SC_range="bigloo"
 SG_range="S G"
-V_range="0 1 2 3 4 5"
-I_range="0 350"
+V_range="0 5"
+I_range="0"
 CC_range="$GCC clang"
 O_range="0 1 2 3"
+TIME=/usr/bin/time
+TIMEARGS="-f real:%e"
 
 if $debug ; then
   # for quick testing
@@ -67,7 +78,7 @@ if $execute ; then
       for SG in $SG_range ; do
         for V in $V_range ; do
           for I in $I_range ; do
-            printf "(%s $SC %s V=%s I=%s)\n" `(time $bench.$SC.$I$SG$V.exe repeat: $repeat $args) 2>&1 | fgrep real | sed -e 's/.*0m//' -e 's/s$//'` $SG $V $I >> $bench.results
+            printf "(%s $SC %s V=%s I=%s)\n" `($TIME $TIMEARGS $bench.$SC.$I$SG$V.exe repeat: $repeat $args) 2>&1 | fgrep real | sed -e 's/.*0m//' -e 's/s$//'` $SG $V $I >> $bench.results
           done
         done
       done
@@ -77,7 +88,7 @@ if $execute ; then
   if test -e $bench.c.c ; then
     for CC in $CC_range ; do
       for O in $O_range ; do
-        printf "(%s $CC -O%s)\n" `(time $bench.$CC.O$O.exe repeat: $repeat $args) 2>&1 | fgrep real | sed -e 's/.*0m//' -e 's/s$//'` $O >> $bench.results
+        printf "(%s $CC -O%s)\n" `($TIME $TIMEARGS $bench.$CC.O$O.exe repeat: $repeat $args) 2>&1 | fgrep real | sed -e 's/.*0m//' -e 's/s$//'` $O >> $bench.results
       done
     done
   fi
