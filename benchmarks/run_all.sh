1fce8bc906f860c60220bc10895dd226e3b3841b
omelancon <ol.melancon@gmail.com>
2023-11-10 19:16:14 -0500

    ready to run all benchmarks!

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 #set -euxo pipefail
 
-files=("recursive/fib"
-       "recursive/ack"
+files=("recursive/ack"
+       "recursive/fib"
        "recursive/fibfp"
        "recursive/tak"
        "recursive/takl"
        "recursive/diviter"
        "recursive/divrec"
        "recursive/array1"
-       "recursive/primes"
-       "recursive/newton"
-       "bglstone/almabench"
-       "bglstone/bague"
-       "bglstone/beval"
-       "bglstone/maze"
-       "bglstone/mbrot"
-       "bglstone/peval"
        "gabriel/browse"
        "gabriel/mazefun"
        "gabriel/nqueens"
@@ -24,20 +16,35 @@ files=("recursive/fib"
        "gabriel/quicksort"
        "gabriel/sum"
        "gabriel/sumfp"
-       "gabriel/triangl")
+       "gabriel/triangl"
+       "bglstone/bague"
+       "bglstone/almabench"
+       # Uses default repeat: 20 in benchmarks.py
+       # So run them last in case one of them takes forever
+       "recursive/primes"
+       "recursive/newton"
+       "bglstone/beval"
+       "bglstone/maze"
+       "bglstone/mbrot"
+       "bglstone/peval")
 
-strategies=("linear"
-            "sametypes"
-            "entropy")
+gambit_strategies=("linear"
+                   "sametypes"
+                   "entropy")
+
+bigloo_strategies=("size")
 
 . ./venv/bin/activate
 
 gambit=../../gambit
 
 for file in ${files[@]}; do
-  for strat in ${strategies[@]}; do
-    python ./benchmark.py -v benchmark ../tests/${file}.scm -g ${gambit} -l 0 1 2 3 -r 3 -m ${strat} -f
-  done
+  # Gambit
+  python ./benchmark.py -v benchmark ../tests/${file}.scm -g ${gambit} -l 0 1 2 3 4 5 -r 50 -m linear -f
+  python ./benchmark.py -v benchmark ../tests/${file}.scm -O -g ${gambit} -l 0 1 2 3 4 5 -r 50 -m linear -f
+  # Bigloo
+  python ./benchmark.py -v benchmark ../tests/${file}.scm -b -l 0 1 2 3 4 5 -r 50 -m size -f
+  python ./benchmark.py -v benchmark ../tests/${file}.scm -O -b -l 0 1 2 3 4 5 -r 50 -m size -f
 done
 
 deactivate
