80709a052d627b33de8f899d9c13f31638d126b7
Manuel Serrano <Manuel.Serrano@inria.fr>
2023-11-14 15:24:26 +0100

    .

--- tests/vector/rev.scm
+++ tests/vector/rev.scm
@@ -1,14 +1,11 @@
-(define (run #!key (n (unknown 4)))
-
-   (define (rev v)
-      (let loop ((i 0) (j (SFX- (vector-length v) 1)))
-	 (if (SFX< i j)
-	     (let ((t (vector-ref v i)))
-		(vector-set! v i (vector-ref v j))
-		(vector-set! v j t)
-		(loop (SFX+ i 1) (SFX- j 1))))))
-
-   (rev (apply vector (iota n))))
+(define (run #!key (v (unknown '#(0 1 2 3 4))))
+   (let loop ((i 0) (j (SFX- (vector-length v) 1)))
+      (if (SFX< i j)
+	  (let ((t (vector-ref v i)))
+	     (vector-set! v i (vector-ref v j))
+	     (vector-set! v j t)
+	     (loop (SFX+ i 1) (SFX- j 1)))
+	  v)))
 
 (define (check result)
-  (equal? result '#(4 3 2 1 0)))
+  (equal? result (apply vector (reverse (iota (vector-length result))))))
