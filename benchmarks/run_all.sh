3eec0e5bcc357436aafe79a00b5c63d0d50c8c89
omelancon <ol.melancon@gmail.com>
2024-01-09 21:10:40 -0500

    update run_all.sh to execute only benchmarks in paper

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -1,38 +1,9 @@
 #!/bin/bash
 #set -euxo pipefail
 
-files=("recursive/ack"
-       "recursive/fib"
-       "recursive/fibfp"
-       "recursive/tak"
-       "recursive/takl"
-       "recursive/diviter"
-       "recursive/divrec"
-       "recursive/array1"
-       "gabriel/browse"
-       "gabriel/boyer"
-       "gabriel/mazefun"
-       "gabriel/nqueens"
-       "gabriel/puzzle"
-       "gabriel/quicksort"
-       "gabriel/sum"
-       "gabriel/sumfp"
-       "gabriel/triangl"
-       "bglstone/bague"
-       "bglstone/almabench"
-       "gambit/chud"
-       # Uses default repeat: 20 in benchmarks.py
-       # So run them last in case one of them takes forever
-       "recursive/primes"
-       "recursive/newton"
-       "bglstone/beval"
-       "bglstone/maze"
-       "bglstone/mbrot"
-       "bglstone/peval")
+files=$(find ../tests/paper -type f -name "*.scm")
 
-gambit_strategies=("linear"
-                   "sametypes"
-                   "entropy")
+gambit_strategies=("linear")
 
 bigloo_strategies=("size")
 
@@ -42,11 +13,11 @@ gambit=../../gambit
 
 for file in ${files[@]}; do
   # Gambit
-  python ./benchmark.py -v benchmark ../tests/${file}.scm -g ${gambit} -l 0 1 2 3 4 5 -r 50 -m linear -f
-  python ./benchmark.py -v benchmark ../tests/${file}.scm -O -g ${gambit} -l 0 1 2 3 4 5 -r 50 -m linear -f
+  python ./benchmark.py -v benchmark ${file} -g ${gambit} -l 0 1 2 3 4 5 -r 10 -m linear -f -t 1800
+  python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l 0 1 2 3 4 5 -r 10 -m linear -f -t 1800
   # Bigloo
-  python ./benchmark.py -v benchmark ../tests/${file}.scm -b -l 0 1 2 3 4 5 -r 50 -m size -f
-  python ./benchmark.py -v benchmark ../tests/${file}.scm -O -b -l 0 1 2 3 4 5 -r 50 -m size -f
+  python ./benchmark.py -v benchmark ${file} -b -l 0 1 2 3 4 5 -r 10 -m size -f -t 1800
+  python ./benchmark.py -v benchmark ${file} -O -b -l 0 1 2 3 4 5 -r 10 -m size -f -t 1800
 done
 
 deactivate
