9a465938e7ba29a42225f18b0bd304e54125584e
omelancon <ol.melancon@gmail.com>
2024-01-11 00:44:53 -0500

    prepare simple benchmark run

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -13,11 +13,11 @@ gambit=../../gambit
 
 for file in ${files[@]}; do
   # Gambit
-  python ./benchmark.py -v benchmark ${file} -g ${gambit} -l 0 1 2 3 4 5 -r 10 -m linear -f -t 1800
-  python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l 0 1 2 3 4 5 -r 10 -m linear -f -t 1800
+  python ./benchmark.py -v benchmark ${file} -g ${gambit} -l 0 1 5 -r 1 -m linear -f -t 1800
+  # python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l 0 1 2 3 4 5 -r 10 -m linear -f -t 1800
   # Bigloo
-  python ./benchmark.py -v benchmark ${file} -b -l 0 1 2 3 4 5 -r 10 -m size -f -t 1800
-  python ./benchmark.py -v benchmark ${file} -O -b -l 0 1 2 3 4 5 -r 10 -m size -f -t 1800
+  # python ./benchmark.py -v benchmark ${file} -b -l 0 1 2 3 4 5 -r 10 -m size -f -t 1800
+  # python ./benchmark.py -v benchmark ${file} -O -b -l 0 1 2 3 4 5 -r 10 -m size -f -t 1800
 done
 
 deactivate
