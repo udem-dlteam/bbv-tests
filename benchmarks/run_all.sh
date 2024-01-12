5219f67eb5cf7e6d87a7369a48c872e2f85543df
omelancon <ol.melancon@gmail.com>
2024-01-11 20:45:40 -0500

    prepare benchmarks

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 #set -euxo pipefail
 
-files=("../tests/paper/micro/rev.scm"
-       "../tests/paper/micro/fibfp.scm"
-       "../tests/paper/micro/tak.scm"
-       "../tests/paper/micro/ack.scm"
-       "../tests/paper/micro/vlen.scm"
-       "../tests/paper/micro/vec.scm"
-       "../tests/paper/micro/primes.scm"
-       "../tests/paper/micro/nqueens.scm"
-       "../tests/paper/macro/conform.scm"
-       "../tests/paper/macro/slatex.scm"
-       "../tests/paper/macro/almabench.scm")
-
-
-#$(find ../tests/paper -type f -name "*.scm")
-
-gambit_strategies=("linear")
-
-bigloo_strategies=("size")
+files=$(find ../tests/paper -type f -name "*.scm")
 
 . ./venv/bin/activate
 
@@ -26,11 +9,11 @@ gambit=../../gambit
 
 for file in ${files[@]}; do
   # Gambit
-  python ./benchmark.py -v benchmark ${file} -g ${gambit} -l 0 1 5 -r 1 -m linear -f -t 1800
-  # python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l 0 1 2 3 4 5 -r 10 -m linear -f -t 1800
+  python ./benchmark.py -v benchmark ${file} -g ${gambit} -l 0 1 2 3 4 8 16 -r 5 -m linear -f -t 1800
+  python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l 0 1 2 3 4 8 16 -r 5 -m linear -f -t 1800
   # Bigloo
-  # python ./benchmark.py -v benchmark ${file} -b -l 0 1 2 3 4 5 -r 10 -m size -f -t 1800
-  # python ./benchmark.py -v benchmark ${file} -O -b -l 0 1 2 3 4 5 -r 10 -m size -f -t 1800
+  python ./benchmark.py -v benchmark ${file} -b ${gambit} -l 0 1 2 3 4 8 16 -r 5 -m size -f -t 1800
+  python ./benchmark.py -v benchmark ${file} -O -b ${gambit} -l 0 1 2 3 4 8 16 -r 5 -m size -f -t 1800
 done
 
 deactivate
