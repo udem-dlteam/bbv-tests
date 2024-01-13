0635db9186a76b920fdbf002f45f103af2a06daf
omelancon <ol.melancon@gmail.com>
2024-01-13 12:01:07 -0500

    extend benchmarks timeout

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -euxo pipefail
 
-files=$(find ../tests/paper -type f -name "*.scm")
+files=../tests/paper/macro/earley.scm #$(find ../tests/paper -type f -name "*.scm")
 
 . ./venv/bin/activate
 
@@ -9,15 +9,15 @@ gambit=../../gambit
 
 for file in ${files[@]}; do
   # Gambit
-  python ./benchmark.py -v benchmark ${file} -g ${gambit} -l 0 1 2 3 4 8 16 -r 5 -f -t 1800
-  python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l 0 1 2 3 4 8 16 -r 5 -f -t 1800
-  python ./benchmark.py -v benchmark ${file} -u -g ${gambit} -l 0 1 2 3 4 8 16 -r 5 -f -t 1800
-  python ./benchmark.py -v benchmark ${file} -u -O -g ${gambit} -l 0 1 2 3 4 8 16 -r 5 -f -t 1800
+  python ./benchmark.py -v benchmark ${file} -g ${gambit} -l 0 1 2 3 4 8 16 -r 5 -f -t 3600
+  python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l 0 1 2 3 4 8 16 -r 5 -f -t 3600
+  python ./benchmark.py -v benchmark ${file} -u -g ${gambit} -l 0 1 2 3 4 8 16 -r 5 -f -t 3600
+  python ./benchmark.py -v benchmark ${file} -u -O -g ${gambit} -l 0 1 2 3 4 8 16 -r 5 -f -t 3600
   # Bigloo
-  python ./benchmark.py -v benchmark ${file} --bigloo -l 0 1 2 3 4 8 16 -r 5 -f -t 1800
-  python ./benchmark.py -v benchmark ${file} -O --bigloo -l 0 1 2 3 4 8 16 -r 5 -f -t 1800
-  python ./benchmark.py -v benchmark ${file} -u --bigloo -l 0 1 2 3 4 8 16 -r 5 -f -t 1800
-  python ./benchmark.py -v benchmark ${file} -u -O --bigloo -l 0 1 2 3 4 8 16 -r 5 -f -t 1800
+  python ./benchmark.py -v benchmark ${file} --bigloo -l 0 1 2 3 4 8 16 -r 5 -f -t 3600
+  python ./benchmark.py -v benchmark ${file} -O --bigloo -l 0 1 2 3 4 8 16 -r 5 -f -t 3600
+  python ./benchmark.py -v benchmark ${file} -u --bigloo -l 0 1 2 3 4 8 16 -r 5 -f -t 3600
+  python ./benchmark.py -v benchmark ${file} -u -O --bigloo -l 0 1 2 3 4 8 16 -r 5 -f -t 3600
 done
 
 deactivate
