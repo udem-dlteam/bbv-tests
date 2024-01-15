cde058c0574ce8aa21251bb88025fb4e1927f5b2
omelancon <ol.melancon@gmail.com>
2024-01-15 10:23:49 -0500

    move bague to micro, add fib to micro and remove vec from micro

---
+++ tests/paper/micro/fib.scm
@@ -0,0 +1,12 @@
+(define (run #!key (n (unknown 39 10)))
+   
+  (define (fib n)
+    (if (SFX< n 2)
+        n
+        (SFX+ (fib (SFX- n 1))
+              (fib (SFX- n 2)))))
+   
+  (fib n))
+
+(define (check result)
+  (equal? result (unknown 63245986 55)))
