6b5d4166e7c362b3ce1d389d2a24d09b4e996dd3
omelancon <ol.melancon@gmail.com>
2024-01-17 14:44:00 -0500

    prepare for final execution before deadline

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -6,18 +6,12 @@ files=$(find ../tests/paper -type f -name "*.scm")
 
 gambit=../../gambit
 
-for limit in 0 4 1 2 3 8 16; do
+for limit in 0 1 2 3 4 5 6 7 8 9 10 20; do
   for file in ${files[@]}; do
     # Gambit
-    python ./benchmark.py -v benchmark ${file} -g ${gambit} -l ${limit} -r 5 -f -t 3600
-    python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l ${limit} -r 5 -f -t 3600
-    python ./benchmark.py -v benchmark ${file} -u -g ${gambit} -l ${limit} -r 5 -f -t 3600
-    python ./benchmark.py -v benchmark ${file} -u -O -g ${gambit} -l ${limit} -r 5 -f -t 3600
+    python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l ${limit} -r 1 -f -t 3600
     # Bigloo
-    python ./benchmark.py -v benchmark ${file} --bigloo -l ${limit} -r 5 -f -t 3600
-    python ./benchmark.py -v benchmark ${file} -O --bigloo -l ${limit} -r 5 -f -t 3600
-    python ./benchmark.py -v benchmark ${file} -u --bigloo -l ${limit} -r 5 -f -t 3600
-    python ./benchmark.py -v benchmark ${file} -u -O --bigloo -l ${limit} -r 5 -f -t 3600
+    python ./benchmark.py -v benchmark ${file} -O --bigloo -l ${limit} -r 1 -f -t 3600
   done
 done
 
