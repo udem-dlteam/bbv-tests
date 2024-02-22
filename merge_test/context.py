0131043ad87d87c9a0f248da45704e45303454c9
omelancon <ol.melancon@gmail.com>
2024-02-22 16:44:36 -0500

    first test in merge simulator

--- merge_test/context.py
+++ merge_test/context.py
@@ -2,6 +2,7 @@ import operator
 
 from enum import auto, Flag
 from functools import total_ordering
+from itertools import pairwise
 from sys import maxsize
 
 
@@ -13,6 +14,9 @@ class NonFixnumType(Flag):
     procedure = auto()
     ret = auto()
 
+    NB_EXTRA_TYPES = 9
+    locals().update({f"t{i}": auto() for i in range(1, NB_EXTRA_TYPES + 1)})
+
     @classmethod
     @property
     def bot(cls):
@@ -22,6 +26,19 @@ class NonFixnumType(Flag):
     @property
     def top(cls):
         return cls(sum(member.value for member in cls))
+
+    def is_top(self):
+        return self == self.top
+
+    def is_bot(self):
+        return self == self.bot
+
+    def __len__(self):
+        return self.value.bit_count()
+
+    def __str__(self):
+        return '|'.join(name[:2] for name in self.name.split('|'))
+
 
 class FixnumOverflow(Exception):
     pass
@@ -121,6 +138,9 @@ class FixnumRange:
     def is_empty(self):
         return self.low > self.high
 
+    def is_top(self):
+        return self == self.top
+
     def __or__(self, other):
         if self.is_empty():
             return other
@@ -129,15 +149,13 @@ class FixnumRange:
         else:
             return FixnumRange(min(self.low, other.low), max(self.high, other.high))
 
-    def _interval_repr(self):
+    def __str__(self):
         if self.is_empty():
-            return "()"
+            return "∅"
+        elif self.is_top():
+            return "fx"
         else:
-            return f"{self.low._value_repr()}, {self.high._value_repr()}"
-
-    def __repr__(self):
-        type_name = type(self).__name__
-        return f"{type_name}({self._interval_repr()})"
+            return f"{self.low._value_repr()}..{self.high._value_repr()}"
 
     def __len__(self):
         return len(range(self.low.value, self.high.value + 1))
@@ -148,23 +166,37 @@ class FixnumRange:
         else:
             return other.low >= self.low and other.high <= self.high
 
+    def __eq__(self, other):
+        if self.is_empty() and other.is_empty():
+            return True
+        else:
+            return self.low == other.low and self.high == other.high
+
 class Type:
     def __init__(self, fixnum_range, types):
         self.range = fixnum_range
         self.types = types
 
-    def _type_repr(self):
-        return f"{self.range._interval_repr()}, {self.types}"
-
-    def __repr__(self):
-        type_name = type(self).__name__
-        return f"{type_name}({self._type_repr()})"
+    def __str__(self):
+        if self.is_top():
+            return "T"
+        elif self.is_bot():
+            return "_"
+        elif self.range.is_empty():
+            return str(self.types)
+        elif self.types.is_bot():
+            return str(self.range)
+        else:
+            return f"{self.range}|{self.types}"
 
     def __contains__(self, other):
         return other.range in self.range and other.types in self.types
 
     def __or__(self, other):
         return Type(self.range | other.range, self.types | other.types)
+
+    def __eq__(self, other):
+        return self.types == other.types and self.range == other.range
 
     @classmethod
     @property
@@ -175,6 +207,12 @@ class Type:
     @property
     def top(cls):
         return cls(FixnumRange.top, NonFixnumType.top)
+
+    def is_top(self):
+        return self == self.top
+
+    def is_bot(self):
+        return self == self.bot
 
 class Context:
     def __init__(self, *types, registers=0, frame=0, closure=0):
@@ -202,18 +240,45 @@ class Context:
 
         return Context(*new_types, registers=len(self.registers), frame=len(self.frame), closure=len(self.closure))
 
-    def size_match(self, other):
-        return len(self.registers) == len(other.registers) \
-           and len(self.frame) == len(other.frame) \
-           and len(self.closure) == len(other.closure)
+    def size_match(self, *others):
+        for other in others:
+            if    len(self.registers) != len(other.registers) \
+               or len(self.frame) != len(other.frame) \
+               or len(self.closure) != len(other.closure):
+               return False
+        return True
 
-    def pretty(self):
-        lines = []
+def format_contexts(contexts):
+    first = contexts[0]
+    if not first.size_match(*contexts[1:]):
+        raise ValueError("Context size does not match")
 
-        for name, container in ("reg", self.registers), ("fra", self.frame), ("clo", self.closure):
-            for i, t in enumerate(container):
-                lines.append(f"{name}[{i}]:{t._type_repr()}")
+    regs = len(first.registers)
+    frame = len(first.frame)
+    closure = len(first.closure)
 
-        return '\n'.join(lines)
-
+    col_names = [f"r{r}" for r in range(1, regs + 1)]
+    col_names += [f"frame[{f}]" for f in range(1, frame + 1)]
+    col_names += [f"clo[{f}]" for c in range(1, closure + 1)]
     
+    rows = [col_names]
+    for context in contexts:
+        rows.append([str(type) for type in context])
+
+    widths = [max(len(rows[i][j]) for i in range(len(rows))) for j in range(len(rows[0]))]
+
+    table = "\n".join(" ; ".join(cell.ljust(width) for width, cell in zip(widths, row)) for row in rows)
+
+    return table
+
+def format_context(context):
+    return format_contexts([context])
+    
+# Common types
+
+fixnums = Type(FixnumRange.top, NonFixnumType.bot)
+non_negative_fixnums = Type(FixnumRange(0, Fixnum.max_fixnum), NonFixnumType.bot)
+not_fixnums = Type(FixnumRange.bot, NonFixnumType.top)
+top = Type.top
+bot = Type.bot
+fixnums_not_maxfixnum = Type(FixnumRange(Fixnum.min_fixnum, Fixnum.max_fixnum - 1), NonFixnumType.bot)
