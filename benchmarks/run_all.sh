48ff1b028ea42b92d22aa037251fe1075f23d12f
omelancon <ol.melancon@gmail.com>
2024-03-05 14:40:26 -0500

    typo

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -13,11 +13,11 @@ for limit in 0 1 2 3 4 5 6 7 8 9 10 20; do
   for file in ${files[@]}; do
     echo skip
     # Gambit
-    #python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l ${limit} -r ${reps} -f -t 3600
-    #python ./benchmark.py -v benchmark ${file} -O -u -g ${gambit} -l ${limit} -r ${reps} -f -t 3600
+    python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l ${limit} -r ${reps} -f -t 3600
+    python ./benchmark.py -v benchmark ${file} -O -u -g ${gambit} -l ${limit} -r ${reps} -f -t 3600
     # Bigloo
-    #python ./benchmark.py -v benchmark ${file} -O --bigloo -l ${limit} -r ${reps} -f -t 3600
-    #python ./benchmark.py -v benchmark ${file} -O -u --bigloo -l ${limit} -r ${reps} -f -t 3600
+    python ./benchmark.py -v benchmark ${file} -O --bigloo -l ${limit} -r ${reps} -f -t 3600
+    python ./benchmark.py -v benchmark ${file} -O -u --bigloo -l ${limit} -r ${reps} -f -t 3600
   done
 done
 
