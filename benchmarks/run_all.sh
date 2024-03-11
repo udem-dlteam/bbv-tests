2452c966d3883da939ba71f657a2f41a674924f3
omelancon <ol.melancon@gmail.com>
2024-03-11 11:08:56 -0400

    remove unsafe execution with BBV from benchmark runs

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -7,22 +7,26 @@ files=$(find ../tests/paper -type f -name "*.scm")
 gambit=../../bbv-gambit
 
 reps=50
+timeout=3600
 
 # BBV runs
 for limit in 0 1 2 3 4 5 6 7 8 9 10 20; do
   for file in ${files[@]}; do
     # Gambit
-    python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l ${limit} -r ${reps} -f -t 3600
-    python ./benchmark.py -v benchmark ${file} -O -u -g ${gambit} -l ${limit} -r ${reps} -f -t 3600
+    python ./benchmark.py -v benchmark ${file} -O -g ${gambit} -l ${limit} -r ${reps} -t ${timeout}
+    
     # Bigloo
-    python ./benchmark.py -v benchmark ${file} -O --bigloo -l ${limit} -r ${reps} -f -t 3600
-    python ./benchmark.py -v benchmark ${file} -O -u --bigloo -l ${limit} -r ${reps} -f -t 3600
+    python ./benchmark.py -v benchmark ${file} -O --bigloo -l ${limit} -r ${reps} -t ${timeout}
   done
 done
 
 for file in ${files[@]}; do
+  # Unsafe runs
+  python ./benchmark.py -v benchmark ${file} -O -u -g ${gambit} -l 0 -r ${reps} -t ${timeout}
+  python ./benchmark.py -v benchmark ${file} -O -u --bigloo -l 0 -r ${reps} -t ${timeout}
+  
   # Gambit with no optimizations at all
-  python ./benchmark.py -v benchmark ${file} -g ${gambit} -l 0 -r 1 -f -t 3600
+  python ./benchmark.py -v benchmark ${file} -g ${gambit} -l 0 -r 1 -t ${timeout}
 done
 
 deactivate
