42c790a3109e4df43e8210aaa646bc09148087e0
omelancon <ol.melancon@gmail.com>
2024-03-25 11:52:47 -0400

    run_all parametrizable verbosity

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -10,33 +10,35 @@ gambit=../../bbv-gambit
 reps=50
 timeout=3600
 
+verbosity="-v"
+
 # Do NodeJS and Chez runs first they should be quick, having no version limits
 for file in ${js_files[@]}; do
-  python ./benchmark.py -v benchmark ${file} -O --node -l 0 -r ${reps} -t ${timeout}
+  python ./benchmark.py $verbosity benchmark ${file} -O --node -l 0 -r ${reps} -t ${timeout}
 done
 
 for file in ${scm_files[@]}; do
-  python ./benchmark.py -v benchmark ${file} -O --chez -l 0 -r ${reps} -t ${timeout}
+  python ./benchmark.py $verbosity benchmark ${file} -O --chez -l 0 -r ${reps} -t ${timeout}
 done
 
 # BBV runs
 for limit in 0 1 2 3 4 5 6 7 8 9 10 15 20; do
   for file in ${scm_files[@]}; do
     # Gambit
-    python ./benchmark.py -v benchmark ${file} -O --gambit ${gambit} -l ${limit} -r ${reps} -t ${timeout}
+    python ./benchmark.py $verbosity benchmark ${file} -O --gambit ${gambit} -l ${limit} -r ${reps} -t ${timeout}
     
     # Bigloo
-    python ./benchmark.py benchmark ${file} -O --bigloo -l ${limit} -r ${reps} -t ${timeout}
+    python ./benchmark.py $verbosity benchmark ${file} -O --bigloo -l ${limit} -r ${reps} -t ${timeout}
   done
 done
 
 for file in ${scm_files[@]}; do
   # Unsafe runs
-  python ./benchmark.py -v benchmark ${file} -O -u --gambit ${gambit} -l 0 -r ${reps} -t ${timeout}
-  python ./benchmark.py -v benchmark ${file} -O -u --bigloo -l 0 -r ${reps} -t ${timeout}
+  python ./benchmark.py $verbosity benchmark ${file} -O -u --gambit ${gambit} -l 0 -r ${reps} -t ${timeout}
+  python ./benchmark.py $verbosity benchmark ${file} -O -u --bigloo -l 0 -r ${reps} -t ${timeout}
   
   # Gambit with no optimizations at all for primitive count
-  python ./benchmark.py -v benchmark ${file} --gambit ${gambit} -l 0 -r 1 -t ${timeout}
+  python ./benchmark.py $verbosity benchmark ${file} --gambit ${gambit} -l 0 -r 1 -t ${timeout}
 done
 
 deactivate
