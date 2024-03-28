076fcb87881274ee4a13cbc8538a43bb3771d626
omelancon <ol.melancon@gmail.com>
2024-03-28 17:02:13 -0400

    write run_profile_guided script for all benchmark

--- benchmarks/run_profile_guided_bbv.sh
+++ benchmarks/run_profile_guided_bbv.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 sleep 5
 
+scm_files=$(find ../tests/paper -type f -regex '^.*macro/[^.]*\.scm$')
+
 . ./venv/bin/activate
 
 gambit=../../bbv-gambit
 
 reps=10
-timeout=1800
+timeout=3600
 
 verbosity="-v"
 
-file=../tests/paper/macro/almabench.scm
-python ./benchmark.py $verbosity profile_guided ${file} --gambit ${gambit} -r ${reps} -t ${timeout}
+for file in ${scm_files[@]}; do
+  python ./benchmark.py $verbosity profile_guided ${file} --gambit ${gambit} -r ${reps} -t ${timeout}
+done
 
 deactivate
