f51cf0db9abc28cd7b9f4afd10cb21ec01f87e2b
omelancon <ol.melancon@gmail.com>
2024-04-17 17:44:47 -0400

    quick run script

--- benchmarks/run_quick_js.sh
+++ benchmarks/run_quick_js.sh
@@ -2,6 +2,7 @@
 sleep 5
 
 js_files=$(find ../tests/paper -type f -regex '^.*/[^.]*\.js$')
+rkt_files=$(find ../tests/paper -type f -regex '^.*/[^.]*\.rkt$')
 
 . ./venv/bin/activate
 
@@ -9,6 +10,10 @@ reps=5
 timeout=3600
 
 verbosity="-v"
+
+for file in ${rkt_files[@]}; do
+  python ./benchmark.py $verbosity benchmark ${file} -O --racket ${racket} -l 0 -r ${reps} -t ${timeout}
+done
 
 for file in ${js_files[@]}; do
   python ./benchmark.py $verbosity benchmark ${file} -O --node -l 0 -r ${reps} -t ${timeout}
