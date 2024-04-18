62cb97f4d08676b1aecd22bf90e9eb5088f23f53
omelancon <ol.melancon@gmail.com>
2024-04-17 20:13:22 -0400

    dead code elimination prevention in Racket

--- racket/main.scm
+++ racket/main.scm
@@ -15,15 +15,15 @@
         (if (pair? args)
             (error 'exec-bench "invalid arguments")
             (let ((args (reverse rev-args)))
-              (let loop2 ((repeat repeat) (result #f))
+              (let loop2 ((repeat repeat) (result '(0)));;;;;;;changé
                 (define (deep-apply align-stack run args)
                   (if (> align-stack 0)
                       (unknown (deep-apply (- align-stack 1) run args))
                       (if (pair? args) (apply run args) (run))))
                 (if (> repeat 0)
-                    (loop2 (- repeat 1) (deep-apply align-stack run args))
+                    (loop2 (- repeat 1) (cons (deep-apply align-stack run args) result));;;;;;;changé
                     (if (and (null? args) (not (check result)))
                         (error 'wrong-result result)
-                        result))))))))
+                        (car result)))))))));;;;;;;changé
 
 (exec-bench)
