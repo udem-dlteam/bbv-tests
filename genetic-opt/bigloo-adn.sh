417c59a747a75a2211fb607b3c77e45883366c8e
Manuel Serrano <Manuel.Serrano@inria.fr>
2024-06-20 21:30:19 +0200

    bigloo adn

--- genetic-opt/bigloo-adn.sh
+++ genetic-opt/bigloo-adn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-benchmarks="micro/ack.scm micro/fib.scm micro/bague.scm micro/fibfp.scm micro/nqueens.scm micro/primes.scm micro/tak.scm"
+#benchmarks="micro/ack.scm micro/fib.scm micro/bague.scm micro/fibfp.scm micro/nqueens.scm micro/primes.scm micro/tak.scm"
+benchmarks="micro/ack.scm"
 adn=$@
 
 base() {
@@ -12,7 +13,7 @@ base() {
   TMPFILE=`mktemp -p $TMPDIR`
 
   cmd="bigloo -srfi arithmeticG -w -unsafe -saw -O3 bbv.bgl ../tests/paper/$bench -copt -DSAW_BBV_STATS=1 -o $TMPFILE.out"
-  #>&2 echo $cmd
+  >&2 echo $cmd
   
   sh -c "$cmd" || exit 1
   result=`$TMPFILE.out 2>&1 | tail +2 | bigloo -eval "(let ((l (port->sexp-list (current-input-port)))) (print (apply + (filter-map (lambda (x) (if (memq (car x) '(ifne add/ov sub/o mul/ov)) (cadr x))) l))) (exit 0))"`
@@ -30,7 +31,7 @@ bbv() {
   TMPFILE=`mktemp -p $TMPDIR`
 
   cmd="BIGLOOBBVSTRATEGY=\"adn\" BIGLOOBBVADN=\"$adn\" BIGLOOBBVVLENGTH=true BIGLOOBBVVERSIONLIMIT=4 bigloo -srfi arithmeticG -w -unsafe -saw -O3 -fsaw-bbv bbv.bgl ../tests/paper/$bench -copt -DSAW_BBV_STATS=1 -o $TMPFILE.out"
-  #>&2 echo $cmd
+  >&2 echo $cmd
   
   sh -c "$cmd" || exit 1
   result=`$TMPFILE.out 2>&1 | tail +2 | bigloo -eval "(let ((l (port->sexp-list (current-input-port)))) (print (apply + (filter-map (lambda (x) (if (memq (car x) '(ifne add/ov sub/o mul/ov)) (cadr x))) l))) (exit 0))"`
@@ -55,4 +56,4 @@ echo $ires
 
 #parallel run ::: $benchmarks
 
-# genetic-opt/genetic-opt.sh -l 8 genetic-opt/bigloo-adn.sh
+# genetic-opt/genetic-opt.sh -l 8 -p genetic-opt/bigloo-adn.sh
