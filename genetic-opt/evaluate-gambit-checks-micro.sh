871a4a7837fb2f91ea62d3d8d9f75127665f4e79
omelancon <ol.melancon@gmail.com>
2024-07-31 13:24:18 -0400

    fix to genetic evaluation script

--- genetic-opt/evaluate-gambit-checks-micro.sh
+++ genetic-opt/evaluate-gambit-checks-micro.sh
@@ -1,17 +1,22 @@
+set -euo pipefail
+
 scm_files=$(find ../tests/paper/micro -type f -regex '^.*/[^.]*\.scm$')
+log_file="evaluate-gambit-checks-micro.log"
 
 gambit=../../bbv-gambit
 
 geomean=1.0
-order=0
+nfiles=$(echo "$scm_files" | wc -l)
+
+params="$@"
+
+echo "=== NEW RUN ON $(date)" >> $log_file
 
 # BBV runs
 for file in ${scm_files[@]}; do
-    order=$((order + 1))
-
     # Gambit
-    out_V0=env BBV_PARAMETERS=$@ ../compile -S gambit -D ${gambit} -V 0 -O3 -f ${file} -P
-    out_V3=env BBV_PARAMETERS=$@ ../compile -S gambit -D ${gambit} -V 3 -O3 -f ${file} -P
+    out_V0=$(env BBV_PARAMETERS="$params" ../compile -S gambit -D ${gambit} -V 0 -O3 -f ${file} -P 2>> $log_file)
+    out_V3=$(env BBV_PARAMETERS="$params" ../compile -S gambit -D ${gambit} -V 3 -O3 -f ${file} -P 2>> $log_file)
 
     checks_V0=$(echo "$out_V0" | grep -oE '\(#.*?[0-9]+\)')
     checks_V3=$(echo "$out_V3" | grep -oE '\(#.*?[0-9]+\)')
@@ -23,7 +28,7 @@ for file in ${scm_files[@]}; do
     geomean=$(echo "$geomean * $ratio" | bc -l)
 done
 
-geometric_mean=$(echo "scale=10; e(l($geomean)/$order)*100" | bc -l)
+geometric_mean=$(echo "scale=10; e(l($geomean)/$nfiles)*100" | bc -l)
 rounded_geometric_mean=$(printf "%.0f" "$geometric_mean")
 
 echo $rounded_geometric_mean
--- genetic-opt/evaluate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This script receives the genome in the parameters and must
-# produce a single integer as output. For example:
-#
-# $ ./evaluate.sh 1 2 3 4 5 6 7 8
-# -646
-
-echo $@ | gsi -e "(let loop ((i (* 1000 (random-real)))) (if (> i 0) (loop (- i 1))))(println (apply (lambda (a b c d e f g h) (- (+ (square a) (square b) (square c) (square d)) (square (+ e f g h)))) (read-all)))"
